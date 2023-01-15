0c77f5f727bc70753eb0a3709d6261e07418311c
Per Helge Vollmo <per.helge.vollmo@gmail.com>
2023-01-15 11:41:37 +0100

    Get input data and load into python

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
 import pandas as pd
+import glob
+
+# Create list to store multiple filepaths
+filepaths = glob.glob('invoices/*.xlsx') # Get everythong with ending xlsx
+print(filepaths)
+
+# Read all files
+for filepath in filepaths:
+    df = pd.read_excel(filepath, sheet_name='Sheet 1')
+    print(df)
