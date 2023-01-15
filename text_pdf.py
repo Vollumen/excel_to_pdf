2e8a38afad6232f40bdc2798da7b479fe099bc33
Per Helge Vollmo <per.helge.vollmo@gmail.com>
2023-01-15 13:45:15 +0100

    Create pdf with headings from txt- corr to one p.

--- text_pdf.py
+++ text_pdf.py
@@ -6,17 +6,19 @@ from pathlib import Path
 # Create list to store multiple filepaths
 filepaths = glob.glob('text_files/*.txt')
 
+# Create ONE pdf file (put outside loop)
+pdf = FPDF(orientation='P', unit='mm', format='A4')
+
 # For loop: Read all fies and create dataframes in python    
 # Print page in pdf for each iteration
 # Extract content form text files on each iteration
 
 for filepath in filepaths:
     df = pd.read_csv(filepath)
-    pdf = FPDF(orientation='P', unit='mm', format='A4')
     pdf.add_page()
     filename = Path(filepath).stem
     heading = filename.split('.')[0]
     heading = heading.title()
     pdf.set_font(family='Times', size=22, style='B')
-    pdf.cell(w=50, h=10, txt=(heading))
-    pdf.output(f'PDF_text/{filename}.pdf')
+    pdf.cell(w=50, h=10, txt=(heading), ln=1)
+pdf.output('output.pdf')
