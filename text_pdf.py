9ec1358d0026873c3c73bb7cbade89f5588a5111
Per Helge Vollmo <per.helge.vollmo@gmail.com>
2023-01-21 15:58:42 +0100

    Add tekst from txt docs to pdf file

--- text_pdf.py
+++ text_pdf.py
@@ -11,8 +11,9 @@ pdf = FPDF(orientation='P', unit='mm', format='A4')
 
 # For loop: Read all fies and create dataframes in python    
 # Print page in pdf for each iteration
-# Extract content form text files on each iteration
+# Extract content from text files on each iteration
 
+# Extract heading from filename
 for filepath in filepaths:
     df = pd.read_csv(filepath)
     pdf.add_page()
@@ -21,4 +22,12 @@ for filepath in filepaths:
     heading = heading.title()
     pdf.set_font(family='Times', size=22, style='B')
     pdf.cell(w=50, h=10, txt=(heading), ln=1)
+
+    # Extract content from text files
+    with open(filepath, 'r') as file:
+        content = file.read()
+        pdf.set_font(family="Times", size=10)
+        pdf.multi_cell(w=0, h=6, txt=content)
+
+
 pdf.output('output.pdf')
